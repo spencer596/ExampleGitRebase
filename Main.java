36c8683450e505f80c2dae31f7f55abb8bcbc6f2
spencer596 <spencer5980@gmail.com>
2024-01-21 13:30:21 -0700

    revision 3 in branch

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main
 {
+    //this is the chronological revision 2 in master
+    //this is the chronological revision 3 in branch
     //this is the chronological revision 6 in master
+
 
     public void methodInMaster()
     {
@@ -8,5 +11,10 @@ public class Main
         System.out.println("Revision 2 in master");
         System.out.println("Revision 4 in master");
         System.out.println("Revision 6 in master");
+    }
+
+    public void methodInBranch()
+    {
+        System.out.println("Revision 3 in branch");
     }
 }
