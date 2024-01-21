a64acd437d5469bcbbd6509073bd476c297c4764
spencer596 <spencer5980@gmail.com>
2024-01-21 13:30:49 -0700

    revision 4 in master

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main
 {
-    //this is the chronological revision 2 in master
+    //this is the chronological revision 4 in master
 
     public void methodInMaster()
     {
         System.out.println("Revision 1 in master");
         System.out.println("Revision 2 in master");
+        System.out.println("Revision 4 in master");
     }
 }
